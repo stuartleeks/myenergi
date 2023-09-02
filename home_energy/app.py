bf47ff1b2219859e01aa951cc2ec9417d91e63f8
Stuart Leeks <stuart@leeks.net>
2023-09-02 15:28:50 +0000

    Add filelock

--- home_energy/app.py
+++ home_energy/app.py
@@ -2,6 +2,7 @@ import datetime
 import os
 from myenergi import MyEnergi
 from dotenv import load_dotenv
+from filelock import Timeout, FileLock
 
 import sqlite3
 
@@ -21,69 +22,76 @@ def main():
         raise ValueError("API_KEY not set")
     
     db_file = os.path.abspath(database_path)
-    print("db_file: ", db_file)
+    print("db_file: ", db_file, flush=True)
     db_path = os.path.dirname(db_file)
-    print("db_path: ", db_path)
+    print("db_path: ", db_path, flush=True)
     if not os.path.exists(db_path):
         print(f"Creating database directory: '{db_path}'")
         os.makedirs(db_path)
     db_path_stat = os.stat(db_path)
-    print ("db path mode: ", oct(db_path_stat.st_mode))
+    print ("db path mode: ", oct(db_path_stat.st_mode), flush=True)
 
-    print(f"Opening database: '{database_path}'")
-    with sqlite3.connect(database_path) as conn:
-        cursor = conn.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS energy (date text, imported real, exported real, generated real, zappi real, self_consumption real, usage real, property_usage real, voltage real, frequency real, PRIMARY KEY (date))")
-        conn.commit()
+    lock_filename=db_file + ".lock"
+    print("Getting file lock: ", lock_filename, flush=True)
 
-        cursor.execute("SELECT MAX(date) FROM energy")
-        res = cursor.fetchall()
-        max_date= res[0][0]
-        print(f"Max date: {max_date}")
+    lock = FileLock("high_ground.txt.lock")
+    with lock:
+        print("Got file lock: ", lock_filename, flush=True)
 
-        if max_date is None:
-            # no data - use default start date
-            if default_start_date:
-                start_date = datetime.datetime.strptime(default_start_date, '%Y-%m-%d').date()
-            else:
-                start_date = datetime.date.today()
-        else:
-            max_date = datetime.datetime.strptime(max_date, '%Y-%m-%d %H:%M:%S')
-            if max_date.hour == 23 and max_date.minute == 59:
-                # Have all of latest day's data - advance a day
-                start_date = max_date.date() + datetime.timedelta(days=1)
-            else:
-                # Still have data to collect for specified day
-                start_date = max_date.date()
-
-        myenergi = MyEnergi(api_key, serial_number)
-        print("Date, Imported, Exported, Generated, Zappi, Usage")
-
-        date = start_date
-        while date <= datetime.date.today():
-            day_data = myenergi.get_day_data(date=date)
-            imported = 0
-            exported = 0
-            generated = 0
-            zappi = 0
-            usage = 0
-            for item in day_data:
-                imported += item.imported
-                exported += item.exported
-                generated += item.generated
-                zappi += item.zappi
-                usage += item.usage
-                cursor.execute("INSERT OR IGNORE INTO energy VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (item.time, item.imported, item.exported, item.generated, item.zappi, item.self_consumption, item.usage, item.property_usage, item.voltage, item.frequency))
-
-            imported = imported / (3600 * 1000)
-            exported = exported / (3600 * 1000)
-            generated = generated / (3600 * 1000)
-            zappi = zappi / (3600 * 1000)
-            usage = usage / (3600 * 1000)
-            print(f"{item.time.date()}, {imported: 0.1f}, {exported: 0.1f}, {generated: 0.1f}, {zappi: 0.1f}, {usage: 0.1f}")
-    
-            date = date + datetime.timedelta(days=1)
+        print(f"Opening database: '{db_file}'", flush=True)
+        with sqlite3.connect(db_file) as conn:
+            cursor = conn.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS energy (date text, imported real, exported real, generated real, zappi real, self_consumption real, usage real, property_usage real, voltage real, frequency real, PRIMARY KEY (date))")
             conn.commit()
+
+            cursor.execute("SELECT MAX(date) FROM energy")
+            res = cursor.fetchall()
+            max_date= res[0][0]
+            print(f"Max date: {max_date}", flush=True)
+
+            if max_date is None:
+                # no data - use default start date
+                if default_start_date:
+                    start_date = datetime.datetime.strptime(default_start_date, '%Y-%m-%d').date()
+                else:
+                    start_date = datetime.date.today()
+            else:
+                max_date = datetime.datetime.strptime(max_date, '%Y-%m-%d %H:%M:%S')
+                if max_date.hour == 23 and max_date.minute == 59:
+                    # Have all of latest day's data - advance a day
+                    start_date = max_date.date() + datetime.timedelta(days=1)
+                else:
+                    # Still have data to collect for specified day
+                    start_date = max_date.date()
+
+            myenergi = MyEnergi(api_key, serial_number)
+            print("Date, Imported, Exported, Generated, Zappi, Usage", flush=True)
+
+            date = start_date
+            while date <= datetime.date.today():
+                day_data = myenergi.get_day_data(date=date)
+                imported = 0
+                exported = 0
+                generated = 0
+                zappi = 0
+                usage = 0
+                for item in day_data:
+                    imported += item.imported
+                    exported += item.exported
+                    generated += item.generated
+                    zappi += item.zappi
+                    usage += item.usage
+                    cursor.execute("INSERT OR IGNORE INTO energy VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (item.time, item.imported, item.exported, item.generated, item.zappi, item.self_consumption, item.usage, item.property_usage, item.voltage, item.frequency))
+
+                imported = imported / (3600 * 1000)
+                exported = exported / (3600 * 1000)
+                generated = generated / (3600 * 1000)
+                zappi = zappi / (3600 * 1000)
+                usage = usage / (3600 * 1000)
+                print(f"{item.time.date()}, {imported: 0.1f}, {exported: 0.1f}, {generated: 0.1f}, {zappi: 0.1f}, {usage: 0.1f}", flush=True)
+        
+                date = date + datetime.timedelta(days=1)
+                conn.commit()
 
 
 if __name__ == "__main__":
